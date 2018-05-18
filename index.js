3cfdff463f0ce53923307b3abae6b29d83240f6b
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-18 18:02:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${[...katzDeliLine, name].length} in line.`;
+  return katzDeli;
 }
