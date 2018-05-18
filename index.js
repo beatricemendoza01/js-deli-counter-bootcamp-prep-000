3fa0ca1fc74e48457975cd56c565c944680904d3
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-18 18:10:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    katzDeliLine.unshift()
+    const firstPerson = katzDeliLine.shift();
+    return firstPerson;
   }
 }
